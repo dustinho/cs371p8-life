72e880fb955953b15bb94ebf11695e5d145f96ef
dustin <dustin@hinagiku.(none)>
2009-11-27 22:51:16 -0600

    fixed memory leak

--- Life.h
+++ Life.h
@@ -12,7 +12,7 @@ template <typename T>
 class Life {
 
     private:
-        vector<vector<T*> > _grid;
+        vector<vector<T> > _grid;
         int generation;
         int population;
         int rows;
@@ -36,11 +36,11 @@ class Life {
                 } else if (linenumber == 1){
                     l.cols = atoi(line.c_str());
                 } else {
-                    vector<T*> row;
+                    vector<T> row;
                     for (int i = 0; i < (int) line.length(); ++i){
                         //cout << "seg now?" << endl;
                         //l._grid[linenumber][i] = new T(line[i]);
-                        row.push_back(new T(line[i]));
+                        row.push_back(T(line[i]));
                         //cout << "seg NOW!" << endl;
                     }
                     l._grid.push_back(row);
@@ -57,7 +57,7 @@ class Life {
             out << "Generation = " << l.generation << ", Population = " << l.population << "." << endl;
             for (int i = 0; i < l.rows; ++i){
                 for (int j = 0; j < l.cols; ++j){
-                    out << l._grid[i][j]->get();
+                    out << l._grid[i][j].get();
                 }
                 out << std::endl;
             }
@@ -69,16 +69,16 @@ class Life {
             // Increment neighbor count of center cells
             for (int r = 1; r < rows-1; ++r) {
                 for (int c = 1; c < cols-1; ++c) {
-                    if (_grid[r][c]->isAlive()) {
-                        _grid[r-1][c]->incNeighbors();
-                        _grid[r][c-1]->incNeighbors();
-                        _grid[r][c+1]->incNeighbors();
-                        _grid[r+1][c]->incNeighbors();
+                    if (_grid[r][c].isAlive()) {
+                        _grid[r-1][c].incNeighbors();
+                        _grid[r][c-1].incNeighbors();
+                        _grid[r][c+1].incNeighbors();
+                        _grid[r+1][c].incNeighbors();
 
-                        _grid[r-1][c-1]->conwayInc();
-                        _grid[r-1][c+1]->conwayInc();
-                        _grid[r+1][c-1]->conwayInc();
-                        _grid[r+1][c+1]->conwayInc();
+                        _grid[r-1][c-1].conwayInc();
+                        _grid[r-1][c+1].conwayInc();
+                        _grid[r+1][c-1].conwayInc();
+                        _grid[r+1][c+1].conwayInc();
                     }
                 }
             }
@@ -86,46 +86,46 @@ class Life {
             // Increment neighbor count of sides
             for (int r = 1; r < rows-1; ++r) {
                 int c = 0;
-                if (_grid[r][c]->isAlive()) {
-                    _grid[r-1][c]->incNeighbors();
-                    _grid[r][c+1]->incNeighbors();
-                    _grid[r+1][c]->incNeighbors();
+                if (_grid[r][c].isAlive()) {
+                    _grid[r-1][c].incNeighbors();
+                    _grid[r][c+1].incNeighbors();
+                    _grid[r+1][c].incNeighbors();
 
-                    _grid[r-1][c+1]->conwayInc();
-                    _grid[r+1][c+1]->conwayInc();
+                    _grid[r-1][c+1].conwayInc();
+                    _grid[r+1][c+1].conwayInc();
                 }
 
                 c = cols - 1;
-                if (_grid[r][c]->isAlive()) {
-                    _grid[r-1][c]->incNeighbors();
-                    _grid[r][c-1]->incNeighbors();
-                    _grid[r+1][c]->incNeighbors();
+                if (_grid[r][c].isAlive()) {
+                    _grid[r-1][c].incNeighbors();
+                    _grid[r][c-1].incNeighbors();
+                    _grid[r+1][c].incNeighbors();
 
-                    _grid[r-1][c-1]->conwayInc();
-                    _grid[r+1][c-1]->conwayInc();
+                    _grid[r-1][c-1].conwayInc();
+                    _grid[r+1][c-1].conwayInc();
                 }
             }
 
             // Increment neighbor count of top sides
             for (int c = 1; c < cols-1; ++c) {
                 int r = 0;
-                if (_grid[r][c]->isAlive()) {
-                    _grid[r][c-1]->incNeighbors();
-                    _grid[r][c+1]->incNeighbors();
-                    _grid[r+1][c]->incNeighbors();
+                if (_grid[r][c].isAlive()) {
+                    _grid[r][c-1].incNeighbors();
+                    _grid[r][c+1].incNeighbors();
+                    _grid[r+1][c].incNeighbors();
 
-                    _grid[r+1][c-1]->conwayInc();
-                    _grid[r+1][c+1]->conwayInc();
+                    _grid[r+1][c-1].conwayInc();
+                    _grid[r+1][c+1].conwayInc();
                 }
 
                 r = rows - 1;
-                if (_grid[r][c]->isAlive()) {
-                    _grid[r-1][c]->incNeighbors();
-                    _grid[r][c-1]->incNeighbors();
-                    _grid[r][c+1]->incNeighbors();
+                if (_grid[r][c].isAlive()) {
+                    _grid[r-1][c].incNeighbors();
+                    _grid[r][c-1].incNeighbors();
+                    _grid[r][c+1].incNeighbors();
 
-                    _grid[r-1][c-1]->conwayInc();
-                    _grid[r-1][c+1]->conwayInc();
+                    _grid[r-1][c-1].conwayInc();
+                    _grid[r-1][c+1].conwayInc();
                 }
             }
 
@@ -135,45 +135,45 @@ class Life {
 
             r = 0;
             c = 0;
-            if (_grid[r][c]->isAlive()) {
-                _grid[r][c+1]->incNeighbors();
-                _grid[r+1][c]->incNeighbors();
+            if (_grid[r][c].isAlive()) {
+                _grid[r][c+1].incNeighbors();
+                _grid[r+1][c].incNeighbors();
 
-                _grid[r+1][c+1]->conwayInc();
+                _grid[r+1][c+1].conwayInc();
             }
 
             r = 0;
             c = cols-1;
-            if (_grid[r][c]->isAlive()) {
-                _grid[r][c-1]->incNeighbors();
-                _grid[r+1][c]->incNeighbors();
+            if (_grid[r][c].isAlive()) {
+                _grid[r][c-1].incNeighbors();
+                _grid[r+1][c].incNeighbors();
 
-                _grid[r+1][c-1]->conwayInc();
+                _grid[r+1][c-1].conwayInc();
             }
 
             r = rows-1;
             c = 0;
-            if (_grid[r][c]->isAlive()) {
-                _grid[r-1][c]->incNeighbors();
-                _grid[r][c+1]->incNeighbors();
+            if (_grid[r][c].isAlive()) {
+                _grid[r-1][c].incNeighbors();
+                _grid[r][c+1].incNeighbors();
 
-                _grid[r-1][c+1]->conwayInc();
+                _grid[r-1][c+1].conwayInc();
             }
 
             r = rows-1;
             c = cols-1;
-            if (_grid[r][c]->isAlive()) {
-                _grid[r-1][c]->incNeighbors();
-                _grid[r][c-1]->incNeighbors();
+            if (_grid[r][c].isAlive()) {
+                _grid[r-1][c].incNeighbors();
+                _grid[r][c-1].incNeighbors();
 
-                _grid[r-1][c-1]->conwayInc();
+                _grid[r-1][c-1].conwayInc();
             }
 
             // Eval each cell
             for (int r = 0; r < rows; ++r) {
                 for (int c = 0; c < cols; ++c) {
-                    _grid[r][c]->eval();
-                    _grid[r][c]->setNeighbors(0);
+                    _grid[r][c].eval();
+                    _grid[r][c].setNeighbors(0);
                 }
             }
         }
