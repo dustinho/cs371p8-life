ac447a5c5f2c3e479bda5e2d3f7fe3f20436cc9c
Jacqueline Tsay <jtsay@obaba.cs.utexas.edu>
2009-11-29 01:52:19 -0600

    added tests

--- TestLife.h
+++ TestLife.h
@@ -30,7 +30,123 @@ struct TestLife : CppUnit::TestFixture {
     // -----
 
     void test_in () {
-		ifstream in("LifeFredkinCell.in");
+		istringstream in("2\n3\n.*.\n0+-\n");
+		Life<Cell> l;
+		in >> l;
+
+
+		CPPUNIT_ASSERT(!l._grid[0][0].isAlive());
+		CPPUNIT_ASSERT(l._grid[0][1].isAlive());
+		CPPUNIT_ASSERT(l._grid[1][0].isAlive());
+		CPPUNIT_ASSERT(l._grid[1][1].isAlive());
+		CPPUNIT_ASSERT(!l._grid[1][2].isAlive());
+	}
+
+	void test_conway_1 () {
+		istringstream in("3\n3\n...\n.*.\n...\n");
+		Life<ConwayCell> l;
+		in >> l;
+
+		l.move();
+
+
+		CPPUNIT_ASSERT(!l._grid[1][1].isAlive());
+	}
+
+	void test_conway_2 () {
+		istringstream in("2\n3\n***\n...\n");
+		Life<ConwayCell> l;
+		in >> l;
+
+		l.move();
+
+		ostringstream out;
+		out << l;
+
+		CPPUNIT_ASSERT(out.str() == "Generation = 1, Population = 2.\n.*.\n.*.\n\n");
+	}
+
+	void test_fredkin_1 () {
+		istringstream in("1\n1\n3\n");
+		Life<FredkinCell> l;
+		in >> l;
+
+		l.move();
+
+		CPPUNIT_ASSERT(l._grid[0][0].age == -1);
+	}
+
+	void test_fredkin_2 () {
+		istringstream in("1\n2\n33\n");
+		Life<FredkinCell> l;
+		in >> l;
+
+		l.move();
+
+		CPPUNIT_ASSERT(l._grid[0][0].age == 4);
+		CPPUNIT_ASSERT(l._grid[0][1].age == 4);
+	}
+
+	void test_fredkin_3 () {
+		istringstream in("1\n2\n+9\n");
+		Life<FredkinCell> l;
+		in >> l;
+
+		l.move();
+
+		ostringstream out;
+		out << l;
+
+		CPPUNIT_ASSERT(l._grid[0][0].age == 11);
+		CPPUNIT_ASSERT(l._grid[0][1].age == 10);
+		CPPUNIT_ASSERT(out.str() == "Generation = 1, Population = 2.\n++\n\n");
+	}
+
+	void test_fredkin_4 () {
+		istringstream in("1\n2\n3-\n");
+		Life<FredkinCell> l;
+		in >> l;
+
+		l.move();
+
+		ostringstream out;
+		out << l;
+
+		CPPUNIT_ASSERT(l._grid[0][0].age == -1);
+		CPPUNIT_ASSERT(l._grid[0][1].age == 0);
+		CPPUNIT_ASSERT(out.str() == "Generation = 1, Population = 1.\n-0\n\n");
+
+		l.move();
+		CPPUNIT_ASSERT(l._grid[0][0].age == 0);
+		CPPUNIT_ASSERT(l._grid[0][1].age == -1);
+	}
+
+	void test_cell_1 () {
+		istringstream in("1\n2\n01\n");
+		Life<Cell> l;
+
+		in >> l;
+
+		l.move();
+
+		ostringstream out;
+		out << l;
+
+		CPPUNIT_ASSERT(out.str() == "Generation = 1, Population = 2.\n1*\n\n");
+	}
+
+	void test_cell_2 () {
+		istringstream in("2\n2\n00\n0*");
+		Life<Cell> l;
+
+		in >> l;
+
+		l.move();
+
+		ostringstream out;
+		out << l;
+
+		CPPUNIT_ASSERT(out.str() == "Generation = 1, Population = 1.\n--\n-*\n\n");
 	}
 
     // -----
@@ -39,6 +155,14 @@ struct TestLife : CppUnit::TestFixture {
 
     CPPUNIT_TEST_SUITE(TestLife);
 	CPPUNIT_TEST(test_in);
+	CPPUNIT_TEST(test_conway_1);
+	CPPUNIT_TEST(test_conway_2);
+	CPPUNIT_TEST(test_fredkin_1);
+	CPPUNIT_TEST(test_fredkin_2);
+	CPPUNIT_TEST(test_fredkin_3);
+	CPPUNIT_TEST(test_fredkin_4);
+	CPPUNIT_TEST(test_cell_1);
+	CPPUNIT_TEST(test_cell_2);
     CPPUNIT_TEST_SUITE_END();};
 
 } // cs
